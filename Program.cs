7215a80f0c5e98c78523df69765cac1e8fc0371b
nicolas lattuada <nicolas.lattuada@gmail.com>
2022-06-25 00:03:01 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,15 @@
-string fileName = "input.txt";
-StreamReader reader;
-if (File.Exists(fileName)) {
-    reader = new StreamReader(fileName);
-} else {
-    reader = new StreamReader(Console.OpenStandardInput());
-}
+StreamReader reader = new StreamReader(Console.OpenStandardInput());
+if (File.Exists("input.txt")) reader = new StreamReader("input.txt");
 StreamWriter writer = new StreamWriter(Console.OpenStandardOutput());
 string ReadLine() => reader.ReadLine()!;
 T Read<T>() => (T)Convert.ChangeType(ReadLine(), typeof(T));
 int ReadInt() => Read<int>();
 string[] Split(char c = ' ') => ReadLine().Split(c);
-IEnumerable<T> Reads<T>(char c = ' ') => from s in Split() select (T)Convert.ChangeType(s, typeof(T));
-T[] ReadArray<T>(char c = ' ') => Reads<T>().ToArray();
-(T, T) ToTwoTuple<T>(IEnumerable<T> e) => (e.First(), e.ElementAt(1));
+IEnumerable<T> Reads<T>(char c = ' ') => from s in Split(c) select (T)Convert.ChangeType(s, typeof(T));
+T[] ReadArray<T>(char c = ' ') => Reads<T>(c).ToArray();
+(T, T) ToTwoTuple<T>(IEnumerable<T> e) => (e.ElementAt(0), e.ElementAt(1));
 (T, T) ReadTwo<T>(char c = ' ') => ToTwoTuple(Reads<T>(c));
-(T, T, T) ToThreeTuple<T>(IEnumerable<T> e) => (e.First(), e.ElementAt(1), e.ElementAt(2));
+(T, T, T) ToThreeTuple<T>(IEnumerable<T> e) => (e.ElementAt(0), e.ElementAt(1), e.ElementAt(2));
 (T, T, T) ReadThree<T>(char c = ' ') => ToThreeTuple(Reads<T>(c));
 void Write<T>(T s) => writer.Write(s);
 void WriteLine<T>(T s) => writer.WriteLine(s);
@@ -30,3 +25,6 @@ while (t-- > 0)
 }
 writer.Flush();
 
+public class Solution
+{
+}
